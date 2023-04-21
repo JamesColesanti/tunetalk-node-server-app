404f46c28b401d137e6b60d6f0d21d019e1abc95
James Colesanti <jamescolesanti@.yahoo.com>
2023-04-21 19:54:22 -0400

    Login support added

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 import express from 'express'
 import cors from 'cors'
+import session from "express-session";
 import ReviewsController from "./controllers/reviews-controller.js";
 import mongoose from "mongoose";
+import UsersController from './controllers/users-controller.js';
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_TUNETALK;
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use(express.json());
+
+UsersController(app);
 ReviewsController(app);
 
 app.listen(process.env.PORT || 4000);
